5919663f6768542103d76caf70bcc41fd87cbd27
dulee <dulee.dev@gmail.com>
2024-01-22 02:32:17 +0900

    Revert ".."
    This reverts commit 65187a36b5439fabbb8efe3d5b3be1c5d4b6118d.

--- src/routes/playground/route/[...catch-all]/index.tsx
+++
@@ -1,9 +0,0 @@
-import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
-
-export default component$(() => {
-  const param = useLocation().params['catch-all'];
-  console.log(param);
-
-  return <></>;
-});
--- types/node.d.ts
+++
@@ -1,5 +0,0 @@
-interface ImportMeta {
-  env: {
-    PUBLIC_STR?: string;
-  };
-}
