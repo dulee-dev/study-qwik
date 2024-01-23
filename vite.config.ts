3760c4c4d8dfafd6fef8be783cf68d314b4ef276
dulee <dulee.dev@gmail.com>
2024-01-23 21:31:19 +0900

    success deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { vanillaExtractPlugin } from "styled-vanilla-extract/vite";
 
 export default defineConfig((): UserConfig => {
